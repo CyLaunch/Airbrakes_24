04df48280c288571d0f22a0eca234dc07ca5dd3e
Softchair <asoftchair@outlook.com>
2024-02-15 18:07:22 -0600

    Added cylogger changes

--- helper_objects/cyllogger.py
+++ helper_objects/cyllogger.py
@@ -8,19 +8,19 @@
 
 from datetime import datetime
 import os
-
+LOGS_DIR = "/home/cylaunch/logs/"
 class cyllogger:
     def __init__(self, name):
         try:
-            self.logfile = open("logs/" + name + datetime.now().strftime("--%Y-%m-%d--%H-%M-%S") + ".txt", "w")
+            self.logfile = open( LOGS_DIR + name + datetime.now().strftime("--%Y-%m-%d--%H-%M-%S") + ".txt", "w")
         except:
-            os.mkdir("logs")
-            self.logfile = open("logs/" + name + datetime.now().strftime("--%Y-%m-%d--%H-%M-%S") + ".txt", "w")
+            os.mkdir("/home/cylaunch/logs/")
+            self.logfile = open( LOGS_DIR + name + datetime.now().strftime("--%Y-%m-%d--%H-%M-%S") + ".txt", "w")
     
     def writeTo(self, message):
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        self.logfile.write("[" + current_time + "] " + message + "\n")
+        self.logfile.write("[" + current_time + "] " + str(message) + "\n")
     
     def __del__(self):
         self.logfile.close()
