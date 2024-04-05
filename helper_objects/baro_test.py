8498970a3bd0604f6eee1a131b86788c5836a7a7
Softchair <bdpope@iastate.edu>
2024-04-05 18:43:00 -0500

    Had to get it off the old branch

--- helper_objects/baro_test.py
+++ helper_objects/baro_test.py
@@ -5,5 +5,13 @@ sensor = barometer()
 start_time = time.time()
 data = sensor.get_altitude()
 end_time = time.time()
-print(data)
-print(str(end_time-start_time))
+data2 = 1138
+while True:
+    start_time = time.time()
+    data = sensor.get_altitude()
+    end_time = time.time()
+    print(data)
+    print(str(end_time-start_time))
+    #print(data2-data)
+    time.sleep(0.1)
+    data2 = data
