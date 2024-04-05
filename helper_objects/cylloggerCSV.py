1eae677fe69e865dcc6c53de6a8c0b0bcfbcb8c9
Bryan Pope <69945940+bdpope402@users.noreply.github.com>
2024-04-05 16:26:26 -0500

    CylLoggerCSV Changes (#10)
    Added Syscalls to CSV writer, small fixes

--- helper_objects/cylloggerCSV.py
+++ helper_objects/cylloggerCSV.py
@@ -1,6 +1,6 @@
 #----------------------------------------------------
 # CyLogger object, simplifies timestamping and 
-# writing to logs. Creates a timestamped logfile .csv excel file with
+# writing to logs. Creates a timestamped logfile .csv file with
 # timestamped entries. Closes the logger upon removal.
 # @Authors Marcus Miller
 # CyLaunch 2023-24
@@ -8,22 +8,21 @@
 
 from datetime import datetime
 import os
-import csv
+
 LOGS_DIR = "/home/cylaunch/logs/"
 class cylloggerCSV:
     def __init__(self, name):
+        self.filePath = LOGS_DIR + name + datetime.now().strftime("--%Y-%m-%d--%H-%M-%S") + ".csv"
         try:    
-            self.logfile = open(LOGS_DIR + name + datetime.now().strftime("--%Y-%m-%d--%H-%M-%S") + ".csv", "w") 
-            self.writer = csv.writer(self.logfile)  
+            self.logfile = os.open(self.filePath, os.O_CREAT | os.O_RDWR | os.O_NONBLOCK) 
         except:
             os.mkdir(LOGS_DIR)
-            self.logfile = open(LOGS_DIR + name + datetime.now().strftime("--%Y-%m-%d--%H-%M-%S") + ".csv", "w")
+            self.logfile = os.open(self.filePath, os.O_CREAT | os.O_RDWR | os.O_NONBLOCK)
     
-    def writeTo(self, message): 
+    def writeToCSV(self, message):
         now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        arr = [current_time, message]
-        self.writer.writerow(arr)
+        current_time = now.strftime("%H:%M:%S.%f")[:-3]
+        os.write(self.logfile,  str.encode("[" + current_time + "]" + ", " + str(message) + "\n"))
     
     def __del__(self):
         self.logfile.close()
